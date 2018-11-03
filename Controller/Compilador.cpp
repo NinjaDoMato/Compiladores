90f5e025ac2d4c800aad049f8094cb0606977255
Daniel Deda <deda.d@hotmail.com>
2018-11-03 11:51:28 -0300

    Added a few more states to Analex.

--- Controller/Compilador.cpp
+++ Controller/Compilador.cpp
@@ -11,6 +11,8 @@ Token Compilador::Analex(char c)
     string lex = " ";
     Arquivo file("", "");
 
+    Token token("NOT_IMP", lex);
+
     switch (state)
     {
         case 0:
@@ -40,6 +42,51 @@ Token Compilador::Analex(char c)
                 lex += c;
                 state = 9;
             }
+            else if(c == '/')
+            {
+                lex += c;
+                state = 10;
+            }
+            else if(c == '%')
+            {
+                lex += c;
+                state = 11;
+            }
+            else if(c == '<')
+            {
+                lex += c;
+                state = 12;
+            }
+            else if(c == '>')
+            {
+                lex += c;
+                state = 13;
+            }
+            else if(c == '=')
+            {
+                lex += c;
+                state = 14;
+            }
+            else if(c == '*')
+            {
+                lex += c;
+                state = 15;
+            }
+            else if(c == '!')
+            {
+                lex += c;
+                state = 16;
+            }
+            else if(c == '&')
+            {
+                lex += c;
+                state = 17;
+            }
+            else if(c == '|')
+            {
+                lex += c;
+                state = 18;
+            }
 
         //break;
 
@@ -59,7 +106,7 @@ Token Compilador::Analex(char c)
             }
             else
             {
-                Token token("L_INT", lex);
+                token = Token("L_INT", lex);
                 return token;
             }
 
@@ -86,7 +133,7 @@ Token Compilador::Analex(char c)
                 lex += c;
             }
 
-            Token token ("L_FLT", lex);
+            token = Token("L_FLT", lex);
             //return token;
 
         case 4:
@@ -111,7 +158,7 @@ Token Compilador::Analex(char c)
             {
                 lex += c;
 
-                Token token("L_CHAR", lex);
+                token = Token("L_CHAR", lex);
                 return token;
             }
             else
@@ -141,16 +188,8 @@ Token Compilador::Analex(char c)
                 c = file.Prox_char();
             }
 
-            if(c == '\"')
-            {
-                lex += c;
-                Token token("L_STR", lex);
-                return token;
-            }
-            else
-            {
-                // TODO:  definir uma mensagem de erro, talvez uma Exception pra ficar legal
-            }
+            lex += c;
+            token = Token("L_STR", lex);
 
         case 8:
 
@@ -160,23 +199,18 @@ Token Compilador::Analex(char c)
             {
                 lex += c;
 
-                Token token("O_INC", lex);
-                return token;
+                token = Token("O_INC", lex);
             }
             else if(c == '=')
             {
                 lex += c;
 
-                Token token("O_MEQ", lex);
-                return token;
+                token = Token("O_MEQ", lex);
             }
             else
             {
-                Token token("O_SUM", lex);
-                return token;
+                token = Token("O_SUM", lex);
             }
-
-        break;
 
         case 9:
 
@@ -186,26 +220,144 @@ Token Compilador::Analex(char c)
             {
                 lex += c;
 
-                Token token("O_SNC", lex);
-                return token;
+                token = Token("O_SNC", lex);
             }
             else if(c == '=')
             {
                 lex += c;
 
-                Token token("O_SEQ", lex);
-                return token;
+                token = Token("O_SEQ", lex);
             }
             else
             {
-                Token token("O_SUB", lex);
-                return token;
+                token = Token("O_SUB", lex);
             }
-
-        break;
 
         case 10:
 
-        break;
+            c = file.Prox_char();
+
+            if(c == '=')
+            {
+                lex += c;
+
+                token = Token("O_DEQ", lex);
+            }
+            else if(c == '/')
+            {
+                lex += c;
+
+                token = Token("T_CMT", lex);
+            }
+            else
+            {
+                token = Token("O_DIV", lex);
+            }
+
+        case 11:
+
+            if(c == '=')
+            {
+                lex += c;
+
+                token = Token("O_REQ", lex);
+            }
+            else
+            {
+                token = Token("O_MOD", lex);
+            }
+
+        case 12:
+
+            if(c == '=')
+            {
+                lex += c;
+
+                token = Token("O_MAE", lex);
+            }
+            else
+            {
+                token = Token("O_SMA", lex);
+            }
+
+        case 13:
+
+            if(c == '=')
+            {
+                lex += c;
+
+                token = Token("O_MEE", lex);
+            }
+            else
+            {
+                token = Token("O_SME", lex);
+            }
+
+        case 14:
+
+            if(c == '=')
+            {
+                lex += c;
+
+                token = Token("O_SEE", lex);
+            }
+            else
+            {
+                token = Token("O_ATR", lex);
+            }
+
+        case 15:
+
+            if(c == '=')
+            {
+                lex += c;
+
+                token = Token("O_MMQ", lex);
+            }
+            else
+            {
+                token = Token("O_MUL", lex);
+            }
+
+        case 16:
+
+            if(c == '=')
+            {
+                lex += c;
+
+                token = Token("O_DIF", lex);
+            }
+            else
+            {
+                token = Token("O_NOT", lex);
+            }
+
+        case 17:
+
+            if(c == '$')
+            {
+                lex += c;
+
+                token = Token("O_AND", lex);
+            }
+            else
+            {
+                // TODO:  definir uma mensagem de erro, talvez uma Exception pra ficar legal
+            }
+
+        case 18:
+
+            if(c == '|')
+            {
+                lex += c;
+
+                token = Token("O_COR", lex);
+            }
+            else
+            {
+                // TODO:  definir uma mensagem de erro, talvez uma Exception pra ficar legal
+            }
     }
+
+    return token;
 }
