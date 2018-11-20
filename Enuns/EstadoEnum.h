aa35b3c6098dbe8b362eafbce6c234c4fd469f92
Daniel Deda <37677288+NinjaDoMato@users.noreply.github.com>
2018-11-19 23:13:28 -0200

    Adicionados mais estados
    Adicionados estados para strings e caracteres.

--- Enuns/EstadoEnum.h
+++ Enuns/EstadoEnum.h
@@ -5,38 +5,40 @@
 #ifndef COMPILADORES_CARACTERENUM_H
 #define COMPILADORES_CARACTERENUM_H
 
-
+// TODO: Enumerações podem ser inicializadas sem valor, assim não temos problema com os valores de cada estado
 enum class EstadoEnum {
 
     // Estado inicial
     INICIAL = 1,
 
     // Estados dos números
-    ESTADO_NUM_1 = 2, // inteiro
-    ESTADO_NUM_2 = 3, // real
-    ESTADO_NUM_3 = 4, // real
+    ESTADO_NUM_1 = 2,       // inteiro
+    ESTADO_NUM_2 = 3,       // real
+    ESTADO_NUM_3 = 4,       // real
 
     // Estado operadores
-    ESTADO_SOMA = 10, // soma
-    ESTADO_SUBTRACAO = 11, // subtracao
-    ESTADO_MENOR = 12, // menor
-    ESTADO_MAIOR = 13, // maior
-    ESTADO_IGUAL = 14, // igual
-    ESTADO_MULTIPLICACAO = 15, // multplicacao
-    ESTADO_PORCENTAGEM = 16, // porcentagem
-    ESTADO_DIVISAO = 17, // divisao
-    ESTADO_DIFERENTE = 18, // diferente
-    ESTADO_E = 19, // and
-    ESTADO_OU = 20, // or
+    ESTADO_SOMA = 10,       // soma
+    ESTADO_SUBTRACAO = 11,  // subtracao
+    ESTADO_MENOR = 12,      // menor
+    ESTADO_MAIOR = 13,      // maior
+    ESTADO_IGUAL = 14,      // igual
+    ESTADO_MULTIPLICACAO = 15,  // multplicacao
+    ESTADO_RESTO = 16,      // resto (porcentagem) <- Antigo ESTADO_DIVISAO repetido
+    ESTADO_DIVISAO = 17,    // divisao
+    ESTADO_DIFERENTE = 18,  // diferente
+    ESTADO_E = 19,          // and
+    ESTADO_OU = 20,         // or
 
     // Estado identificador
     ESTADO_IDENTIFICADOR_1 = 21,
-    ESTADO_IDENTIFICADOR_2 = 22
-
-
-
-
-
+    ESTADO_IDENTIFICADOR_2 = 22,
+    
+    // Estado letras
+    ESTADO_STR_1 = 23,      // string
+    ESTADO_STR_2 = 24,      // string
+    ESTADO_CHAR_1 = 25,     // char
+    ESTADO_CHAR_2 = 26      // char
+    
 };
 
 
